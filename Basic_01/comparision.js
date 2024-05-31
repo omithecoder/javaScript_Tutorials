dd7cd243c5ed4fd7c1d5a6d801406fed72717c7a
omithecoder <omkarsohamkhanvilkar@gmail.com>
2024-05-31 17:26:36 +0530

    operations

---
+++ Basic_01/comparision.js
@@ -0,0 +1,32 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 >= 1)
+
+
+// following type of comparison convert the string into a number and checks the value and compare it
+console.log("2" == 2) 
+console.log("1" > 2)
+
+
+
+// comparison operator >,<,>=,<= convert the null => 0 and then compare
+// equal to operator not convert null into 0 it consider as a empty object
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// undefiend compariaion => false
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+
+// strictly equal
+
+let s = "2"
+let n = 2
+console.log(s === n); // false     since "===" this operator checks the variables strictly means along with value it checks the datatype also
+
+console.log(s == n); //true    since "==" this operator only checks for value
---
+++ Basic_01/operations.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let a = 10;
+let b = 2;
+
+
+// Arithmatic Operations
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// Implicit type conversions
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log(1 + 2 + 2)
+
+// when we log string first and after that wants to log numbers with addition operation then it convert all the numbers into string and convert the addition operator into concatination operator
+
+
+let c =  10
+console.log(c++) //first print =>  10 and after increment by 1  => 11
+c = 10
+console.log(++c) // first increment by 1 and print => 11
+
+
+
+
